56247e34529bda45b4e2b9f4c72bc3f90d3e2d08
phosphenesdoodles <152762333+phosphenesdoodles@users.noreply.github.com>
2024-12-02 22:05:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Function to toggle the visibility of the text box when the cover is clicked
+function toggleInfo(coverElement) {
+  // Toggle the "active" class on the clicked cover section
+  coverElement.classList.toggle('active');
+}
+
